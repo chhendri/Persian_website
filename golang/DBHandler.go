1be16c8da2a64dabd0bd0d7950a4c8a87bea39e9
Chris Adam <chris.adam@b12-consulting.com>
2022-10-09 13:00:26 +0200

    Prepared deployment WIP

--- golang/DBHandler.go
+++ golang/DBHandler.go
@@ -7,6 +7,7 @@ import (
   "database/sql"
   "fmt"
   "log"
+  "os"
   //"html/template"
   //"encoding/json"
   //"bytes"
@@ -14,15 +15,21 @@ import (
 
 var db *sql.DB
 
+func getEnv(key, fallback string) string {
+    if value, ok := os.LookupEnv(key); ok {
+        return value
+    }
+    return fallback
+}
 
 func connectDB(w http.ResponseWriter, r *http.Request) {
     // Capture connection properties.
     cfg := mysql.Config{
-        User:   "Persian_dvl",
-        Passwd: "password",
+        User:   getEnv("MYSQL_USER", "Persian_dvl"),
+        Passwd: getEnv("MYSQL_ROOT_PASSWORD", "password"),
         Net:    "tcp",
-        Addr:   "127.0.0.1:3306",
-        DBName: "Persian",
+        Addr:   getEnv("MYSQL_HOST", "127.0.0.1:3306"),
+        DBName: getEnv("MYSQL_DATABASE", "Persian"),
     }
     // Get a database handle.
     var err error
